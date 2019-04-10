f5ac5170358d5483eef17bc83b0b40a6606a7ebe
Dogac Akyıldız <ndakyildiz@gmail.com>
2019-04-10 10:10:53 +0300

    Create exes-and-ohs.cs

---
+++ exes-and-ohs.cs
@@ -0,0 +1,26 @@
+/*
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. 
+The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+*/
+
+// My Solution
+
+using System.Linq;
+using System;
+public static class Kata 
+{
+  public static bool XO (string input)
+  {
+     return input.ToLower().Count(i => i == 'x') == input.ToLower().Count(i => i == 'o');
+  }
+}
+
+// Since returning value is boolean, we can write a is equal statement.
