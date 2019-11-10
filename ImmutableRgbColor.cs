182c0353cc72cf6d1a8eda96ddaa813698609c59
Dogac Akyıldız <ndakyildiz@gmail.com>
2019-11-11 01:11:49 +0300

    Create ImmutableRgbColor.cs

---
+++ ImmutableRgbColor.cs
@@ -0,0 +1,63 @@
+class RGBColor
+    {
+        public int Red { get; }
+        public int Green { get; }
+        public int Blue { get; }
+
+        public RGBColor(int blue, int green, int red)
+        {
+            if (blue > 255 || blue < 0 || 
+                green > 255 || green < 0 ||
+                red > 255 || red < 0)
+                throw new ArgumentException();
+
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+
+        public override string ToString()
+        {
+            return $"Red : {Red}, Green: {Green}, Blue: {Blue}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            RGBColor other = obj as RGBColor;
+
+            return this.Red == other.Red &&
+                   this.Green == other.Green &&
+                   this.Blue == other.Blue;
+        }
+
+        public static bool operator ==(RGBColor first, RGBColor second)
+        {
+            if (object.ReferenceEquals(first, null))
+                return object.ReferenceEquals(second, null);
+
+            return first.Equals(second);
+        }
+
+        public static bool operator !=(RGBColor first, RGBColor second)
+        {
+            return !(first == second);
+        }
+
+        protected bool Equals(RGBColor other)
+        {
+            return Red == other.Red && 
+                   Green == other.Green && 
+                   Blue == other.Blue;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Red;
+                hashCode = (hashCode * 397) ^ Green;
+                hashCode = (hashCode * 397) ^ Blue;
+                return hashCode;
+            }
+        }
+    }
