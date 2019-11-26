4fdb259469e6efd4bc57c2a04b512092b17b7216
Dogac Akyıldız <ndakyildiz@gmail.com>
2019-11-27 00:47:45 +0300

    Use the Null-Coalescing Operator.

--- LazyLoadingProperty.cs
+++ LazyLoadingProperty.cs
@@ -5,11 +5,6 @@ private string Password
 {
     get
     {
-        if (_password == null)
-        {
-            _password = _password ?? (_password = CallExpensiveOperation());
-        }
-
-        return _password;
+        return _password ?? (_password = CallExpensiveOperation());
     }
 }
