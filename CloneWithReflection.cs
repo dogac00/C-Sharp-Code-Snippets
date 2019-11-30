c14c2701ae763bd0da8e0d70d643c93feb9a9dd3
Dogac Akyıldız <ndakyildiz@gmail.com>
2019-11-30 13:37:09 +0300

    Update CloneWithReflection.cs

--- CloneWithReflection.cs
+++ CloneWithReflection.cs
@@ -14,6 +14,11 @@ namespace CloneWithReflection
                                     .GetType()
                                     .GetMethod("MemberwiseClone", BindingFlags.NonPublic | BindingFlags.Instance);
 
+            // Note that this is a shallow copy,
+            // value types in the object are bitwise copied
+            // and reference types are copied but
+            // the values that references point to are not copied
+            // because of this, original object may affect the cloned object and vice versa
             StringBuilder cloned = cloneMethod.Invoke(sb, null) as StringBuilder;
         }
     }
