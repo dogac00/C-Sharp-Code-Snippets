0ad6a4fd9b3de59db95abc2e32b45b42c059b71a
Dogac Akyıldız <ndakyildiz@gmail.com>
2020-09-02 00:07:33 +0300

    Update and rename dateutil.cs to GetMonthDifference.cs

--- GetMonthDifference.cs
+++ GetMonthDifference.cs
@@ -5,10 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using ideal.Pools;
 
+// A code snippet from our ideal integrator
 namespace ideal
 {
     public static class DateUtil
     {
+        // Differences are cached
+        // symbol1 is like F_AKBNK0820
+        // symbol2 is like F_AKBNK1020
+        // month difference here is 2
         public static int GetMonthDifference(string symbol1, string symbol2)
         {
             return MonthDifferencePool.GetMonthDifference(symbol1, symbol2);
