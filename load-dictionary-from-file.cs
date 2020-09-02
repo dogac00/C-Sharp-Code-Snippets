a66eb119416c3234a7e9822689eb4f134af2193e
Dogac Akyıldız <ndakyildiz@gmail.com>
2020-09-03 01:43:48 +0300

    Update load-dictionary-from-file.cs

--- load-dictionary-from-file.cs
+++ load-dictionary-from-file.cs
@@ -29,11 +29,30 @@ namespace ideal
             return JsonConvert.DeserializeObject<Dictionary<string, double>>(jsonText);
         }
 
+        // This can be optimized
+        // Currently, there are a lot of unnecessary heap-allocations here
         public static Dictionary<string, double> LoadTeminatDictionary(string path = TxtPath)
         {
             return File.ReadAllLines(path)
                        .Select(l => l.Split(','))
                        .ToDictionary(x => x[0].Trim(), x => Convert.ToDouble(x[1].Trim()));
         }
+        
+        public static Dictionary<string, double> LoadDictionaryOptimized(string path)
+        {
+            var lines = File.ReadAllLines(path);
+            var dict = new Dictionary<string, double>();
+            foreach (var line in lines)
+            {
+                int idx = line.IndexOf(',');
+                var first = line.AsSpan(0, idx);
+                var second = line.AsSpan(idx + 1, line.Length - idx - 1);
+                double.TryParse(second, out var secondDbl);
+                dict[first.ToString()] = secondDbl;
+            }
+        
+            return dict;
+        }
+
     }
 }
